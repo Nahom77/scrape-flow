7a622a7c0d6be341c75635e82297b6f6a9e6171d
Nahom77 <nahomtigistu777@gmail.com>
2026-02-23 13:45:03 +0300

    feat: home layout page created

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import { Separator } from "@/components/ui/separator";
+import React, { ReactNode } from "react";
+
+function layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="h-screen flex">
+      <div className="min-h-screen flex flex-col flex-1">
+        <header className="h-12.5 px-6 py-4 flex justify-between items-center container">
+          ScrapeFlow
+        </header>
+        <Separator />
+        <div className="overflow-auto">
+          <div className="py-4 flex-1 text-accent-foreground container">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default layout;
