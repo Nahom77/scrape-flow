4ea0f7b9e0fb4ce72c325d96d44844c5d0f2e70c
Nahom77 <nahomtigistu777@gmail.com>
2026-03-02 17:04:09 +0300

    refactor: comment out unused Zustand store implementation

--- store/store.ts
+++ store/store.ts
@@ -1,14 +1,7 @@
-import { User } from "@/types";
-import { create } from "zustand";
+// import { create } from "zustand";
 
-interface AppState {
-  user: User | undefined;
-  setUser: (user: User | undefined) => void;
-  logOut: () => void;
-}
+// interface AppState {
+// }
 
-export const useAppStore = create<AppState>()((set) => ({
-  user: undefined,
-  setUser: (user) => set({ user }),
-  logOut: () => set({ user: undefined }),
-}));
+// export const useAppStore = create<AppState>()((set) => ({
+// }));
