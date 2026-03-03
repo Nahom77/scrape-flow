19c2c87a0ef363462221ee01695ac442ab16fcdd
Nahom77 <nahomtigistu777@gmail.com>
2026-03-03 14:56:36 +0300

    feat: implement GetWorkflowsForUser function to retrieve user workflows

--- actions/workflows/getWorkflowsForUser.ts
+++ actions/workflows/getWorkflowsForUser.ts
@@ -1,3 +1,20 @@
 "use server";
 
-export async function GetWorkflowsForUser() {}
+import { getServerSession } from "@/lib/get-session";
+import prisma from "@/lib/prisma";
+
+export async function GetWorkflowsForUser() {
+  const session = await getServerSession();
+  if (!session?.user) {
+    throw new Error("unauthenticated");
+  }
+
+  return prisma.workflow.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+}
