a69494775b8bb7e43a3703b0e22a81646f0a8259
Nahom77 <nahomtigistu777@gmail.com>
2026-03-09 11:35:35 +0300

    feat: single workflow retreival action

--- actions/workflows/getWorkflowsForUser.ts
+++ actions/workflows/getWorkflowsForUser.ts
@@ -18,3 +18,17 @@ export async function GetWorkflowsForUser() {
     },
   });
 }
+
+export async function GetSingleWorkflow(workflowId: string) {
+  const session = await getServerSession();
+  if (!session?.user) {
+    throw new Error("unauthenticated");
+  }
+
+  return prisma.workflow.findUnique({
+    where: {
+      id: workflowId,
+      userId: session.user.id,
+    },
+  });
+}
