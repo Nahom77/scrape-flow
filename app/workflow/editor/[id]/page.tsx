76144bac82fd4baddb52a8154bc40f290a30bed2
Nahom77 <nahomtigistu777@gmail.com>
2026-03-09 11:47:40 +0300

    refactor: toggling of themes

--- app/workflow/editor/[id]/page.tsx
+++ app/workflow/editor/[id]/page.tsx
@@ -1,7 +1,14 @@
+import { GetSingleWorkflow } from "@/actions/workflows/getWorkflowsForUser";
+
 async function page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
+  const workflow = await GetSingleWorkflow(id);
 
-  return <div>{id} hello</div>;
+  if (!workflow) {
+    return <div>Workflow not found</div>;
+  }
+
+  return <div>{workflow?.name} hello</div>;
 }
 
 export default page;
--- app/workflow/layout.tsx
+++ app/workflow/layout.tsx
@@ -1,3 +1,6 @@
+import Logo from "@/components/Logo";
+import { ModeToggle } from "@/components/ThemeModeToggle";
+import { Separator } from "@/components/ui/separator";
 import { getServerSession } from "@/lib/get-session";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
@@ -9,7 +12,17 @@ async function layout({ children }: { children: ReactNode }) {
     redirect("/sign-in");
   }
 
-  return <div>{children}</div>;
+  return (
+    <div className="w-full h-screen flex flex-col">
+      {" "}
+      {children}
+      <Separator />
+      <footer className="p-2 flex justify-between items-center">
+        <Logo iconSize={16} fontSize="text-xl" />
+        <ModeToggle />
+      </footer>
+    </div>
+  );
 }
 
 export default layout;
