0dedf17ac75b46e7aef6728ad94b7c6c379d0ac9
Nahom77 <nahomtigistu777@gmail.com>
2026-03-24 15:50:53 +0300

    feat: useExecutionplan hook and execution plan helper function created

---
+++ lib/workflow/executionPlan.ts
@@ -0,0 +1,66 @@
+import { AppNode } from "@/types/app-node.type";
+import {
+  WorkflowExecutionPlan,
+  WorkflowExecutionPlanPhase,
+} from "@/types/workflow.type";
+import { Edge, getIncomers } from "@xyflow/react";
+import { TaskRegistry } from "@/lib/workflow/task/registry";
+
+type FlowToExecutionPlanType = {
+  executionPlan?: WorkflowExecutionPlan;
+};
+
+export function FlowToExecutionPlan(
+  nodes: AppNode[],
+  edges: Edge[],
+): FlowToExecutionPlanType {
+  const entryPoint = nodes.find(
+    (node) => TaskRegistry[node.data.type].isEntryPoint,
+  );
+  if (!entryPoint) {
+    throw new Error("TODO");
+  }
+
+  const planned = new Set<string>();
+  const executionPlan: WorkflowExecutionPlan = [
+    {
+      phase: 1,
+      nodes: [entryPoint],
+    },
+  ];
+
+  for (
+    let phase = 2;
+    phase <= nodes.length || planned.size < nodes.length;
+    phase++
+  ) {
+    const nextPhase: WorkflowExecutionPlanPhase = { phase, nodes: [] };
+
+    for (const currentNode of nodes) {
+      if (planned.has(currentNode.id)) continue;
+
+      const invalidInputs = getInvalidInputs(currentNode, edges, planned);
+
+      if (invalidInputs.length > 0) {
+        const incomers = getIncomers(currentNode, nodes, edges);
+        if (incomers.every((incomer) => planned.has(incomer.id))) {
+          console.log("invalid inputs");
+          throw new Error("TODO");
+        } else {
+          continue;
+        }
+      }
+
+      nextPhase.nodes.push(currentNode);
+      planned.add(currentNode.id);
+    }
+  }
+
+  return { executionPlan };
+}
+
+function getInvalidInputs(node: AppNode, edges: Edge[], planned: Set<string>) {
+  const invalidInputs: string[] = [];
+
+  return invalidInputs;
+}
