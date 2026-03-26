ed839bac2670eeef5d3ead57d435eb60faee172a
Nahom77 <nahomtigistu777@gmail.com>
2026-03-26 15:21:41 +0300

    fix: execution plan

--- app/workflow/_components/topbar/ExecuteBtn.tsx
+++ app/workflow/_components/topbar/ExecuteBtn.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import useExecutionPlan from "@/hooks/useExecutionPlan";
 import { PlayIcon } from "lucide-react";
 import React from "react";
 
 function ExecuteBtn({ workflowId }: { workflowId: string }) {
+  const generate = useExecutionPlan();
   return (
-    <Button variant={"outline"}>
+    <Button
+      variant={"outline"}
+      onClick={() => {
+        const plan = generate();
+        console.log("---Plan---");
+        console.table(plan);
+      }}
+    >
       <PlayIcon size={16} className="stroke-orange-400" />
       Execute{" "}
     </Button>
