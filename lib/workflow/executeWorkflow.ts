729af9ac1eb9ba9f310162a2e6ebf57422f4f38d
Nahom77 <nahomtigistu777@gmail.com>
2026-04-09 16:18:32 +0300

    feat: enhance workflow execution by integrating environment management and updating LaunchBrowserExecutor

--- lib/workflow/executeWorkflow.ts
+++ lib/workflow/executeWorkflow.ts
@@ -9,6 +9,7 @@ import { ExecutionPhase } from "@/generated/prisma/client";
 import { AppNode } from "@/types/app-node.type";
 import { TaskRegistry } from "./task/registry";
 import { ExecutorRegistry } from "./executor/registry";
+import { Environment, ExecutionEnvironment } from "@/types/executor.type";
 
 export async function ExecuteWorkflow(executionId: string) {
   const execution = await prisma.workflowExecution.findUnique({
@@ -25,7 +26,7 @@ export async function ExecuteWorkflow(executionId: string) {
     throw new Error("Execution not found");
   }
 
-  const environment = { phases: {} };
+  const environment: Environment = { phases: {} };
 
   await initializeWorkflowExecution(executionId, execution.workflowId);
   await initializePhaseStatuses(execution);
@@ -34,7 +35,7 @@ export async function ExecuteWorkflow(executionId: string) {
   let executionFailed = false;
 
   for (const phase of execution.phases) {
-    const phaseExecution = await executeWorkflowPhase(phase);
+    const phaseExecution = await executeWorkflowPhase(phase, environment);
     if (!phaseExecution.success) {
       executionFailed = true;
       break;
@@ -123,9 +124,14 @@ async function finalizeWorkflowExecution(
     .catch(() => {});
 }
 
-async function executeWorkflowPhase(phase: ExecutionPhase) {
+async function executeWorkflowPhase(
+  phase: ExecutionPhase,
+  environment: Environment,
+) {
   const startedAt = new Date();
   const node = JSON.parse(phase.node) as AppNode;
+
+  setUpEnvironmentForPhase(node, environment);
 
   await prisma.executionPhase.update({
     where: {
@@ -140,7 +146,7 @@ async function executeWorkflowPhase(phase: ExecutionPhase) {
   const creditsRequried = TaskRegistry[node.data.type].credits;
   console.log(`executing phase ${phase.name}`);
 
-  const success = await executePhase(phase, node);
+  const success = await executePhase(phase, node, environment);
 
   await finalizePhase(phase.id, success);
   return { success };
@@ -165,11 +171,36 @@ async function finalizePhase(phaseId: string, success: boolean) {
 async function executePhase(
   phase: ExecutionPhase,
   node: AppNode,
+  environment: Environment,
 ): Promise<boolean> {
   const runFn = ExecutorRegistry[node.data.type];
   if (!runFn) {
     return false;
   }
 
-  return await runFn();
+  const executionEnvironment: ExecutionEnvironment = createExecutionEnvironment(
+    node,
+    environment,
+  );
+
+  return await runFn(executionEnvironment);
+}
+
+function setUpEnvironmentForPhase(node: AppNode, environment: Environment) {
+  environment.phases[node.id] = { inputs: {}, outputs: {} };
+  const inputs = TaskRegistry[node.data.type].inputs;
+
+  for (const input of inputs) {
+    const inputValue = node.data.inputs[input.name];
+    if (inputValue) {
+      environment.phases[node.id].inputs[input.name] = inputValue;
+      continue;
+    }
+  }
+}
+
+function createExecutionEnvironment(node: AppNode, environment: Environment) {
+  return {
+    getInput: (name: string) => environment.phases[node.id].inputs[name],
+  };
 }
--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -1,4 +1,22 @@
-export async function LaunchBrowserExecutor(): Promise<boolean> {
-  console.log("running launc browser executor");
-  return true;
+import { ExecutionEnvironment } from "@/types/executor.type";
+import puppeteer from "puppeteer";
+
+export async function LaunchBrowserExecutor(
+  environment: ExecutionEnvironment,
+): Promise<boolean> {
+  try {
+    const websiteUrl = environment.getInput("Website Url");
+    console.log(websiteUrl);
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+
+    setTimeout(async () => {
+      await browser.close();
+    }, 3000);
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
