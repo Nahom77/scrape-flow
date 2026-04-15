560cdb6670c5f2cd24227e271fc42cd06f0e58b5
Nahom77 <nahomtigistu777@gmail.com>
2026-04-15 13:05:33 +0300

    feat: implement ExecutionsPage and add GetWorkflowExecutions action for fetching workflow runs

---
+++ actions/workflows/getWorkflowExecutions.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { getServerSession } from "@/lib/get-session";
+import prisma from "@/lib/prisma";
+
+export async function GetWorkflowExecutions(workflowId: string) {
+  const session = await getServerSession();
+  if (session?.user) {
+    throw new Error("unauthenticated");
+  }
+
+  return await prisma.workflowExecution.findMany({
+    where: {
+      workflowId,
+      userId: session?.user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
--- app/workflow/runs/[workflowId]/page.tsx
+++ app/workflow/runs/[workflowId]/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Topbar from "../../_components/topbar/Topbar";
+import { GetWorkflowExecutions } from "@/actions/workflows/getWorkflowExecutions";
 
-function ExecutionsPage() {
-  return <div>ExecutionsPage</div>;
+async function ExecutionsPage({
+  params,
+}: {
+  params: Promise<{ workflowId: string }>;
+}) {
+  const workflowId = (await params).workflowId;
+  return (
+    <div className="w-full h-full overflow-auto">
+      <Topbar
+        workflowId={workflowId}
+        title="All runs"
+        subtitle="List of all your workflow runs"
+        hideButtons
+      />
+      <ExecutionsTable workflowId={workflowId} />
+    </div>
+  );
 }
 
 export default ExecutionsPage;
+
+async function ExecutionsTable({ workflowId }: { workflowId: string }) {
+  const executions = await GetWorkflowExecutions(workflowId);
+  if (!executions) {
+    return <div>No Data</div>;
+  }
+}
