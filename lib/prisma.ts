abdddd51c91f11ff311ac2b4715a213b76af4665
Nahom77 <nahomtigistu777@gmail.com>
2026-05-16 11:52:18 +0300

    feat: add middleware to create user balance on user creation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -17,4 +17,16 @@ const prisma =
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
+// @ts-expect-error - to by pass the any issue
+prisma.$use(async (params, next) => {
+  const result = await next(params);
+  if (params.model === "User" && params.action === "create") {
+    // create balance row; credits has default 1000 in schema
+    await prisma.userBalance
+      .create({ data: { userId: result.id } })
+      .catch(() => {});
+  }
+  return result;
+});
+
 export default prisma;
